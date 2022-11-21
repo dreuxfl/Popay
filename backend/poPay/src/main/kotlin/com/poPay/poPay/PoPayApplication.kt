22eb8d4a4eff8806238977346fe4e987f52ef403
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-21 10:42:42 +0100

    Added initial backend

---
+++ backend/poPay/src/main/kotlin/com/poPay/poPay/PoPayApplication.kt
@@ -0,0 +1,11 @@
+package com.poPay.poPay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PoPayApplication
+
+fun main(args: Array<String>) {
+	runApplication<PoPayApplication>(*args)
+}
