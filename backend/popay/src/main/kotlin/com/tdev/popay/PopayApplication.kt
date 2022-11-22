8d9eb53f0956a68872d05129c1882243c76335d6
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-22 17:06:31 +0100

    Modified base project

---
+++ backend/popay/src/main/kotlin/com/tdev/popay/PopayApplication.kt
@@ -0,0 +1,11 @@
+package com.tdev.popay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PopayApplication
+
+fun main(args: Array<String>) {
+	runApplication<PopayApplication>(*args)
+}
