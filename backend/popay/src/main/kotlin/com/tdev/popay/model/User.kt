03ceb40ab3e3fed290cdf5af61fafdae848d37bd
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-22 19:34:50 +0100

    User API working fine

--- backend/popay/src/main/kotlin/com/tdev/popay/model/User.kt
+++ backend/popay/src/main/kotlin/com/tdev/popay/model/User.kt
@@ -1,10 +1,11 @@
 package com.tdev.popay.model
 
+import org.hibernate.Hibernate
 import javax.persistence.*
 import javax.validation.constraints.*
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +18,20 @@ data class User(
     val email: String = "",
     @get: NotBlank
     val password: String = "",
-    @get: NotBlank
-    val wallet: Double = 0.0,
-)
+    val wallet: Double? = 0.0,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as User
+
+        return id_user == other.id_user
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id_user = $id_user , first_name = $first_name , last_name = $last_name , email = $email , password = $password , wallet = $wallet )"
+    }
+}
