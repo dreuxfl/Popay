b612dd41672b055a12c750c286d400c73170910b
Sébastien Cartoux <sebastien.cartoux@epitech.eu>
2022-11-22 16:47:35 +0100

    Improved base backend

---
+++ backend/popay/src/test/java/com/tdev/popay/PopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdev.popay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
