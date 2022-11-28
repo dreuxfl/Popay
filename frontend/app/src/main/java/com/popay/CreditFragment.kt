ab01de32ecac56a195a8ae6b75da28fca0386986
dreuxfl <florent.dreux@epitech.eu>
2022-11-28 15:53:59 +0100

    Nav bar OK

--- frontend/app/src/main/java/com/popay/CreditFragment.kt
+++ frontend/app/src/main/java/com/popay/CreditFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ScanFragment : Fragment() {
+class CreditFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        return inflater.inflate(R.layout.fragment_credit, container, false)
     }
 
 }
