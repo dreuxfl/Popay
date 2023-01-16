654e2de36c3369c2d85772b990f0d81f1727f58d
Wael Fezari <wael.fezari@epitech.eu>
2023-01-16 13:37:13 +0100

    Cart Details

---
+++ frontend/app/src/main/java/com/popay/CartDetails.kt
@@ -0,0 +1,11 @@
+package com.popay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_details)
+    }
+}
