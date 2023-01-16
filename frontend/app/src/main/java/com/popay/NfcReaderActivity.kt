5ad49e7bf3b17c04860f95c6149a971dca7ad3f0
Wael Fezari <wael.fezari@epitech.eu>
2023-01-16 15:34:03 +0100

    Error message fix

--- frontend/app/src/main/java/com/popay/NfcReaderActivity.kt
+++ frontend/app/src/main/java/com/popay/NfcReaderActivity.kt
@@ -100,7 +100,7 @@ class NfcReaderActivity : AppCompatActivity()
                 }, {
                     val intent1 = Intent(this, MainActivity::class.java)
                     startActivity(intent1)
-                    Toast.makeText(this, "Server Error", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "transaction failed - insufficient funds", Toast.LENGTH_LONG).show()
                 }
             ) {
                 override fun getHeaders(): MutableMap<String, String> {
