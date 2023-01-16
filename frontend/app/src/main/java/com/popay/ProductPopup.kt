3ddb4444ecc12a0a14bc9016eda741188c098a62
dreuxfl <florent.dreux@epitech.eu>
2023-01-16 12:57:51 +0100

    Changed button text and added profile to navigation menu

--- frontend/app/src/main/java/com/popay/ProductPopup.kt
+++ frontend/app/src/main/java/com/popay/ProductPopup.kt
@@ -15,10 +15,7 @@ import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.view.animation.DecelerateInterpolator
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
